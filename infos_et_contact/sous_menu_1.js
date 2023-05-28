115f685575483415493d7ed8481adfd14b63eb8b
Nathan <nathan@gadbin.com>
2023-05-28 02:12:14 +0200

    loader + chrono fonctionne sur toutes les pages

--- infos_et_contact/sous_menu_1.js
+++ infos_et_contact/sous_menu_1.js
@@ -1,10 +1,48 @@
 function main(){
 
+    chrono()
     disableButton()
     setInterval(disableButton,1000)         //Il y a t'il un autre moyen de voir si disableButton est vrai (sans l'appeler toutes les secondes)
 
 }
     
+function clock() {
+
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
+    
+    document.getElementById("clock").innerHTML = inf10(hours) + ":" + inf10(minutes) + ":" + inf10(seconds)  
+    
+}
+
+function chrono(){
+        let actual_time = new Date()
+
+        setInterval(()=>{
+            let time = new Date()
+    
+            time = new Date(time - actual_time)
+            let hours = time.getHours()
+            let minutes = time.getMinutes()
+            let seconds = time.getSeconds()
+            
+            clock()
+
+            document.getElementById("chrono").innerHTML = inf10(hours-1) + ":" + inf10(minutes) + ":" + inf10(seconds) 
+
+        },1000)
+}
+
+
+
+function inf10(time) {
+    if (time < 10) {
+        time = '0' + time
+    }
+    return time;
+}
 
 function disableButton(){
 
